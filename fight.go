c5d458ecf6949898049b4f63d5646e986bee8258
Gabor Goczan <goczangabor24@gmail.com>
2026-09-20 21:55:51 +0200

    Fight function refactor

--- fight.go
+++ fight.go
@@ -9,143 +9,167 @@ import (
 )
 
 func fight(cfg *config, args ...string) error {
-
-	if len(args) == 0 || len(args) == 1 {
+	if len(args) < 2 {
 		fmt.Println("\nFirst enter the Pokemon you want to fight, then one of your own Pokemon from your Pokedex")
 		return nil
 	}
 
-	p1 := args[0]
-	p2 := args[1]
+	enemyName := args[0]
+	playerName := args[1]
 
-	if cfg.currentPokemon[p1] != 1 {
+	if !validFight(cfg, enemyName, playerName) {
+		return nil
+	}
+
+	enemy, err := getPokemonForFight(cfg, enemyName)
+	if err != nil {
+		return err
+	}
+
+	addEnemyStats(cfg, enemy)
+
+	playerWon := runBattle(cfg, enemyName, playerName)
+
+	if playerWon {
+		catchPokemonAfterFight(cfg, enemy)
+		levelUpPokemon(cfg, playerName)
+	}
+
+	delete(cfg.pokemonToFight, enemyName)
+
+	return nil
+}
+
+func validFight(cfg *config, enemyName, playerName string) bool {
+	if cfg.currentPokemon[enemyName] != 1 {
 		fmt.Println("No such Pokemon in the area.\nCheck the available Pokemon with 'current pokemon'")
-		return nil
+		return false
 	}
 
-	_, ok := cfg.pokedex[p2]
-	if !ok {
-		fmt.Println("Pick a Pokemon you've already caught.\nHint: check your Pokedex")
-		return nil
+	if _, ok := cfg.pokedex[playerName]; !ok {
+		fmt.Println("Pick a Pokemon you've already caught.\nHint: check you Pokedex")
+		return false
 	}
 
-	_, oke := cfg.pokedex[p1]
-	if oke {
+	if _, ok := cfg.pokedex[enemyName]; ok {
 		fmt.Println("You've already caught this Pokemon")
-		return nil
+		return false
 	}
 
-	if p1 == p2 {
-		fmt.Println("You've already caught this Pokemon")
-		return nil
-	}
+	return true
+}
 
-	url := "https://pokeapi.co/api/v2/pokemon/" + args[0]
+func getPokemonForFight(cfg *config, name string) (*pokemonDetails, error) {
+	url := "https://pokeapi.co/api/v2/pokemon/" + name
 
-	var pokemon1 *pokemonDetails
+	var pokemon *pokemonDetails
 
 	cachedRes, ok := cfg.cache.Get(url)
 	if ok {
-		if err := json.Unmarshal(cachedRes, &pokemon1); err != nil {
-			return err
+		if err := json.Unmarshal(cachedRes, &pokemon); err != nil {
+			return nil, err
 		}
-	} else {
-		res, err := http.Get(url)
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close()
-
-		data, err := io.ReadAll(res.Body)
-		if err != nil {
-			return err
-		}
-
-		if err := json.Unmarshal(data, &pokemon1); err != nil {
-			return err
-		}
-		cfg.cache.Add(url, data)
+		return pokemon, nil
 	}
 
-	cfg.pokemonToFight[pokemon1.Name] = make(map[string]int)
-	for _, stat := range pokemon1.Stats {
-		cfg.pokemonToFight[pokemon1.Name][stat.Stat.Name] = stat.BaseStat
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
 	}
 
-	enemyHp := cfg.pokemonToFight[p1]["hp"]
-	enemyAttack := cfg.pokemonToFight[p1]["attack"]
-	enemyDefense := cfg.pokemonToFight[p1]["defense"]
+	defer res.Body.Close()
 
-	playerHp := cfg.pokemonStats[p2]["hp"]
-	playerAttack := cfg.pokemonStats[p2]["attack"]
-	playerDefense := cfg.pokemonStats[p2]["defense"]
+	data, err := io.ReadAll(res.Body)
 
-	maxHealthp2 := cfg.pokemonStats[p2]["hp"]
-
-	status := func() {
-		fmt.Println()
-		if enemyHp < 0 {
-			fmt.Printf("\nEnemy: 0\n")
-		} else {
-			fmt.Printf("\nEnemy: %v\n", enemyHp)
-		}
-
-		if playerHp < 0 {
-			fmt.Printf("\nPlayer: 0\n")
-		} else {
-			fmt.Printf("Player: %v\n", playerHp)
-		}
-		time.Sleep(500 * time.Millisecond)
+	if err != nil {
+		return nil, err
 	}
+
+	if err := json.Unmarshal(data, &pokemon); err != nil {
+		return nil, err
+	}
+
+	cfg.cache.Add(url, data)
+
+	return pokemon, nil
+}
+
+func addEnemyStats(cfg *config, pokemon *pokemonDetails) {
+	cfg.pokemonToFight[pokemon.Name] = make(map[string]int)
+
+	for _, stat := range pokemon.Stats {
+		cfg.pokemonToFight[pokemon.Name][stat.Stat.Name] = stat.BaseStat
+	}
+}
+
+func runBattle(cfg *config, enemyName, playerName string) bool {
+	enemyHp := cfg.pokemonToFight[enemyName]["hp"]
+	enemyAttack := cfg.pokemonToFight[enemyName]["attack"]
+	enemyDefense := cfg.pokemonToFight[enemyName]["defense"]
+
+	playerHp := cfg.pokemonStats[playerName]["hp"]
+	playerAttack := cfg.pokemonStats[playerName]["attack"]
+	playerDefense := cfg.pokemonStats[playerName]["defense"]
 
 	for enemyHp > 0 && playerHp > 0 {
 
-		if enemyAttack < playerDefense && playerAttack < enemyDefense {
-			time.Sleep(1 * time.Second)
-			fmt.Println("\nLong fight, none of them seems to be able to defeat the other...")
-			time.Sleep(1 * time.Second)
-			fmt.Println("Draw\n")
-			return nil
+		playerDamage := playerAttack - enemyDefense
+		if playerDamage < 1 {
+			playerDamage = 1
+		}
+		enemyHp -= playerDamage
+		if enemyHp <= 0 {
+			printFightStatus(enemyHp, playerHp)
+			fmt.Println("Player won")
+			return true
 		}
 
-		if playerAttack-enemyDefense < 0 {
-		} else {
-			enemyHp -= playerAttack - enemyDefense
-			if enemyHp <= 0 {
-				break
-			}
+		enemyDamage := enemyAttack - playerDefense
+		if enemyDamage < 1 {
+			enemyDamage = 1
 		}
+		playerHp -= enemyDamage
+		printFightStatus(enemyHp, playerHp)
+	}
+	fmt.Println("\nCPU won")
+	return false
 
-		if enemyAttack-playerDefense < 0 {
-			status()
-		} else {
-			playerHp -= enemyAttack - playerDefense
-			status()
-			if playerHp <= 0 {
-				break
-			}
-		}
+}
+
+func printFightStatus(enemyHp, playerHp int) {
+	if enemyHp < 0 {
+		enemyHp = 0
 	}
 
-	if enemyHp <= 0 {
-		fmt.Println("Player won")
-		fmt.Printf("\n%v caught!", pokemon1.Name)
-		cfg.pokedex[pokemon1.Name] = pokemon1
-		cfg.pokemonStats[pokemon1.Name] = make(map[string]int)
-		for _, stat := range pokemon1.Stats {
-			cfg.pokemonStats[pokemon1.Name][stat.Stat.Name] = stat.BaseStat
+	if playerHp < 0 {
+		playerHp = 0
+	}
+
+	fmt.Printf("\nEnemy: %v\n", enemyHp)
+	fmt.Printf("Player: %v\n", playerHp)
+
+	time.Sleep(500 * time.Millisecond)
+}
+
+func catchPokemonAfterFight(cfg *config, pokemon *pokemonDetails) {
+	fmt.Printf("\n%v caught!", pokemon.Name)
+
+	cfg.pokedex[pokemon.Name] = pokemon
+	cfg.pokemonStats[pokemon.Name] = make(map[string]int)
+
+	for _, stat := range pokemon.Stats {
+		cfg.pokemonStats[pokemon.Name][stat.Stat.Name] = stat.BaseStat
+	}
+
+	cfg.pokemonStats[pokemon.Name]["Base Experience"] = pokemon.BaseExperience
+}
+
+func levelUpPokemon(cfg *config, pokemonName string) {
+	for stat := range cfg.pokemonStats[pokemonName] {
+		if stat == "Base Experience" {
+			continue
 		}
-		cfg.pokemonStats[pokemon1.Name]["Base Experience"] = pokemon1.BaseExperience
-	} else {
-		fmt.Println("\nCPU won")
-	}
-	fmt.Println()
 
-	delete(cfg.pokemonToFight, p1)
-	playerHp = maxHealthp2
-	for stat := range cfg.pokemonStats[p2] {
-		cfg.pokemonStats[p2][stat] += 1
+		cfg.pokemonStats[pokemonName][stat]++
 	}
-
-	return nil
 }
